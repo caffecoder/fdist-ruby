bdd2607d08dfd00d1f61c6ffde0911ec73bbe8cd
Adam Kubica <caffecoder@kaizen-step.com>
2013-04-27 20:49:14 +0200

    Corrected indentation, fixed tests.

--- src/file_distribution.rb
+++ src/file_distribution.rb
@@ -9,47 +9,47 @@ require "ftools"
 #
 class FileDistribution
 
-    # Creates new instance with directory prefix.
-    #
-    # Params:
-    # - prefix: directory prefix.
-    def initialize(prefix)
-        @ext = '.dat'
-        @path = File.expand_path(prefix)
-    end
+  # Creates new instance with directory prefix.
+  #
+  # Params:
+  # - prefix: directory prefix.
+  def initialize(prefix)
+    @ext = '.dat'
+    @path = File.expand_path(prefix)
+  end
 
-    # Params:
-    # - ext: file extension.
-    def set_extension(ext)
-        if ext.length > 0 && ext.chars.first != '.'
-            @ext = '.' + ext
-        else
-            @ext = ext
-        end
+  # Params:
+  # - ext: file extension.
+  def set_extension(ext)
+    if ext.length > 0 && ext.chars.first != '.'
+      @ext = '.' + ext
+    else
+      @ext = ext
     end
+  end
 
-    # Returns Destination path.
-    def get_path
-        @path
-    end
+  # Returns Destination path.
+  def get_path
+    @path
+  end
 
-    # Params:
-    # - id: database file ID etc.
-    def hex_path(id)
-        hex = "%x" % id
-        hex = '0%s' % hex if hex.length % 2 != 0
-        @path = File.join(@path,hex.scan(/../))
-        @path += @ext
-    end
+  # Params:
+  # - id: database file ID etc.
+  def hex_path(id)
+    hex = "%x" % id
+    hex = '0%s' % hex if hex.length % 2 != 0
+    @path = File.join(@path,hex.scan(/../))
+    @path += @ext
+  end
 
-    # Params:
-    # - path: source file path.
-    #
-    # Raises a SystemCallError if the file cannot be renamed.
-    def rename_from(path)
-        dst_dir = File.dirname(@path)
-        File.mkpath(dst_dir) unless File.exists?(dst_dir)
+  # Params:
+  # - path: source file path.
+  #
+  # Raises a SystemCallError if the file cannot be renamed.
+  def rename_from(path)
+    dst_dir = File.dirname(@path)
+    File.mkpath(dst_dir) unless File.exists?(dst_dir)
 
-        File.rename(path,@path)
-    end
+    File.rename(path,@path)
+  end
 end
--- src/file_distribution_test.rb
+++ src/file_distribution_test.rb
@@ -3,55 +3,63 @@
 #
 
 require "file_distribution"
+require 'fileutils'
 require "test/unit"
 
 class TestFileDistribution < Test::Unit::TestCase
  
   def setup
-  	@fd = FileDistribution.new("/tmp//")
+    if not File.exists?("/tmp/ridler")
+      FileUtils.mkdir("/tmp/ridler")
+    end
+
+    @fd = FileDistribution.new("/tmp/ridler//")
   end
 
   def test_path
-  	assert_equal("/tmp",@fd.get_path())
+    assert_equal("/tmp/ridler", @fd.get_path())
   end 
 
   def test_case1
-	@fd.set_extension("tmp")
-	@fd.set_extension(".dat")
-	@fd.hex_path(102423)
-	assert_equal("/tmp/01/90/17.dat",@fd.get_path())
+    @fd.set_extension("tmp")
+    @fd.set_extension(".dat")
+    @fd.hex_path(102423)
+    assert_equal("/tmp/ridler/01/90/17.dat", @fd.get_path())
   end 
 
   def test_case2
-	@fd.set_extension("dat")
-	@fd.hex_path(256)
-	assert_equal("/tmp/01/00.dat",@fd.get_path())
+    @fd.set_extension("dat")
+    @fd.hex_path(256)
+    assert_equal("/tmp/ridler/01/00.dat", @fd.get_path())
   end
 
   def test_case3
-	@fd.set_extension("")
-	@fd.hex_path(256)
-	assert_equal("/tmp/01/00",@fd.get_path())
+    @fd.set_extension("")
+    @fd.hex_path(256)
+    assert_equal("/tmp/ridler/01/00", @fd.get_path())
   end 
 
   def test_case4
-	@fd.hex_path(1)
-	assert_equal("/tmp/01.dat",@fd.get_path())
+    @fd.hex_path(1)
+    assert_equal("/tmp/ridler/01.dat", @fd.get_path())
   end 
 
   def test_case5
-	f = File.open('/tmp/test.txt', 'w')
-	f.close()
-	
-	assert_equal(true,File.exists?("/tmp/test.txt"))
+    f = File.open('/tmp/ridler/test.txt', 'w')
+    f.close()
 
-	@fd.set_extension(".dat")
-	@fd.hex_path(256)
+    assert_equal(true, File.exists?("/tmp/ridler/test.txt"))
 
-	@fd.rename_from("/tmp/test.txt")
-	assert_equal(true,File.exists?("/tmp/01/00.dat"))
+    @fd.set_extension(".dat")
+    @fd.hex_path(256)
+
+    @fd.rename_from("/tmp/ridler/test.txt")
+    assert_equal(true, File.exists?("/tmp/ridler/01/00.dat"))
   end 	
 
   def teardown
+    if File.exists?("/tmp/ridler")
+      FileUtils.rm_r("/tmp/ridler")
+    end
   end
 end
